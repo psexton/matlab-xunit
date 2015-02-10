80362ff2b7d62fdc88202e4b0c86572af5e27e0b
James Myatt <james.myatt@tessella.com>
2015-02-10 14:57:27 +0000

    Update tests readme

--- tests/Readme.m
+++ tests/Readme.m
@@ -1,7 +1,7 @@
-%   This directory contains the test suite for the mUnit test framework.  Before
+%   This directory contains the test suite for the xUnit test framework.  Before
 %   running the test suite, do the following:
 %
-%   1. Make sure the mUnit test framework directory is on your path
+%   1. Make sure the xUnit test framework directories are on your path
 %   2. Make sure the helper_classes subdirectory of the test directory is on
 %      your path.
 %   3. Make the test directory your current directory.

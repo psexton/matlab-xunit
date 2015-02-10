e2ebb2f3bfbcb7c31e6f9d5eadd08c74738fbab5
James Myatt <james.myatt@tessella.com>
2015-02-10 14:17:02 +0000

    Display extra information for MultipleErrors exception

--- src/utilities/displayFault.m
+++ src/utilities/displayFault.m
@@ -18,6 +18,13 @@ displayStack(filterStack(exception.stack), ...
     file_handle);
 fprintf(file_handle, '\n%s\n', exception.message);
 
+% Include more info on MultipleErrors
+if strcmp(exception.identifier, 'MATLAB:MException:MultipleErrors')
+    for i = 1:numel(exception.cause)
+        fprintf(file_handle, 'Cause #%.0f:\t%s\n', i, exception.cause{i}.message);
+    end
+end
+
 fprintf(file_handle, '\n');
 
 end
